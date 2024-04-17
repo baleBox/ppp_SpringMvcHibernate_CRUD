dfa9d070dceb86995e977595df14bfc218f295f6
Alexander Stupin <astupin2012@gmail.com>
2024-04-17 18:16:22 +0300

    First commit.

--- src/main/java/balebox/springmvc/model/User.java
+++ src/main/java/balebox/springmvc/model/User.java
@@ -1,8 +1,6 @@
 package balebox.springmvc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
+import javax.persistence.*;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
